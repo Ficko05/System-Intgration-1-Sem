7d6e57a17cf7e92c659449df4014f062075d7e8b
cybon <kimon5@hotmail.dk>
2020-09-07 21:15:15 +0200

    sd

--- soft2020fall-si-master/code/P2-TCPS/src/tcps/TCPS.java
+++ soft2020fall-si-master/code/P2-TCPS/src/tcps/TCPS.java
@@ -1,8 +1,6 @@
 package tcps;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -23,12 +21,14 @@ import java.util.Scanner;
  * The steps 3 and 4 can be repeated many times depending on the protocol agreed between the server and the client.
  */
 
-public class TCPS {
+public class TCPS
+{
     public static final int PORT = 6666;
     public static ServerSocket serverSocket = null; // Server gets found
     public static Socket openSocket = null;         // Server communicates with the client
 
-    public static Socket configureServer() throws UnknownHostException, IOException {
+    public static Socket configureServer() throws UnknownHostException, IOException
+    {
         // get server's own IP address
         String serverIP = InetAddress.getLocalHost().getHostAddress();
         System.out.println("Server ip: " + serverIP);
@@ -43,69 +43,59 @@ public class TCPS {
         return openSocket;
     }
 
+    public static void connectClient(Socket openSocket) throws IOException
+    {
+        String request, response;
+
+        // two I/O streams attached to the server socket:
+        Scanner in;         // Scanner is the incoming stream (requests from a client)
+        PrintWriter out;    // PrintWriter is the outcoming stream (the response of the server)
+        in = new Scanner(openSocket.getInputStream());
+        out = new PrintWriter(openSocket.getOutputStream(),true);
+        // Parameter true ensures automatic flushing of the output buffer
+
+        // Server keeps listening for request and reading data from the Client,
+        // until the Client sends "stop" requests
+        while(in.hasNextLine())
+        {
+            request = in.nextLine();
+
+            if(request.equals("stop"))
+            {
+                out.println("Good bye, client!");
+                System.out.println("Log: " + request + " client!");
+                break;
+            }
+            else
+            {
+                // server responses
+                response = new StringBuilder(request).reverse().toString();
+                out.println(response);
+                // Log response on the server's console, too
+                System.out.println("Log: " + response);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException
     {
-
-        openSocket = configureServer();
-        while(true)
+        try
         {
-            try
-            {
-                new ClientHandler(serverSocket.accept()).start();
-            }
-            catch(Exception e)
-            {
-                System.out.println(" Connection fails: " + e);
-            }
-            finally
-            {
-                openSocket.close();
-                System.out.println("Connection to client closed");
-
-                serverSocket.close();
-                System.out.println("Server port closed");
-            }
+            openSocket = configureServer();
+            connectClient(openSocket);
         }
+        catch(Exception e)
+        {
+            System.out.println(" Connection fails: " + e);
+        }
+        finally
+        {
+            openSocket.close();
+            System.out.println("Connection to client closed");
+
+            serverSocket.close();
+            System.out.println("Server port closed");
+        }
+
     }
-
-    public class ClientHandler extends Thread
-    {
-        private Socket clientSocket;
-        private PrintWriter out;
-        private BufferedReader in;
-
-        public ClientHandler(Socket socket)
-        {
-            this.clientSocket = socket;
-        }
-
-        @Override
-        public void run()
-        {
-            try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    if (".".equals(inputLine)) {
-                        out.println("bye");
-                        break;
-                    }
-                    out.println(inputLine);
-                }
-
-                in.close();
-                out.close();
-                clientSocket.close();
-            }
-            catch (IOException ex) {
-                // Handle exception
-            }
-
-        }
-    }
-
-
 }
