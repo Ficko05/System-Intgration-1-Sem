967d7acad8e28c9d0549dd6d6528df74ff364583
sebbelebbe <sebbelebbe8930@gmail.com>
2020-09-08 13:53:56 +0200

    can send an image via udp from client to server but not the otherway around

--- soft2020fall-si-master/code/P3-UDPC/src/dk/dd/udpc/UDPClient.java
+++ soft2020fall-si-master/code/P3-UDPC/src/dk/dd/udpc/UDPClient.java
@@ -1,5 +1,10 @@
 package dk.dd.udpc;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,31 +21,44 @@ public class UDPClient
        
     // buffers for the messages
     public static String message;
-    private static byte[] dataIn = new byte[256];
-    private static byte[] dataOut = new byte[256];  
+    private static byte[] dataIn = new byte[64768];
+    private static byte[] dataOut = new byte[64768];
     
     // In UDP messages are encapsulated in packages and sent over sockets
     private static DatagramPacket requestPacket;    
     private static DatagramPacket responsePacket;  
     private static DatagramSocket clientSocket;
-    
+
+
+
+
+
+
     public static void main(String[] args) throws IOException
     {
         // Enter server's IP address as a parameter from Run/Edit Configuration/Application/Program Arguments
         clientSocket = new DatagramSocket(); 
         InetAddress serverIP = InetAddress.getByName(args[0]);
         System.out.println(serverIP);
-        
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Type message: ");
+            BufferedImage img = ImageIO.read(new File("C:/Users/sebastian/Documents/skole/System intergration/Assignement 1/System-Intgration-1-Sem/soft2020fall-si-master/code/P3-UDPC/src/test.jpg"));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(img, "jpg", baos);
+            baos.flush();
+            byte[] buffer = baos.toByteArray();
+            //System.out.println(buffer.toString());
 
-        while((message = scan.nextLine()) != null)
-        {
-            sendRequest(serverIP);            
-            receiveResponse();
-        }
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverIP, serverPort);
+        System.out.println(buffer + " " + " " + buffer.length);
+            clientSocket.send(packet);
+
+
+            //sendRequest(serverIP);
+            //receiveResponse();
+
         clientSocket.close(); 
     }
+
+
     
     public static void sendRequest(InetAddress serverIP) throws IOException
     {
