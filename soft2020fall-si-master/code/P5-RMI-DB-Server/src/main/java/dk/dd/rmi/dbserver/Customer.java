4332a7eb3969cba8bfba5eeacc112d46afd97d94
Ficko05 <34158635+Ficko05@users.noreply.github.com>
2020-09-15 20:40:00 +0200

    Add files via upload

---
+++ soft2020fall-si-master/code/P5-RMI-DB-Server/src/main/java/dk/dd/rmi/dbserver/Customer.java
@@ -0,0 +1,40 @@
+package dk.dd.rmi.dbserver;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Customer
+{
+    @Id
+    private Long accnum;
+    @NonNull
+    private String name;
+    @NonNull
+    private Double amount;
+
+    public void setAccnum(Long accnum) {
+        this.accnum = accnum;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getAccnum() { return this.accnum; }
+
+    public String getName() { return this.name; }
+
+    public Double getAmount() { return this.amount; }
+}
